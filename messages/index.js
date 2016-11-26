5b68a3f461f685cc77641bd04da9f9aa9da77e42
Robert Wildman <Robertwildman4@gmail.com>
2016-11-26 22:32:46 +0000

    Changing to mp4

--- messages/index.js
+++ messages/index.js
@@ -22,8 +22,8 @@ bot.dialog('/', function (session) {
 
 		giphy.translate(text, function(err, res) {
  			var message = new builder.Message(session).attachments([{
-                contentType: "image/gif",
-                contentUrl: res.data.images.downsized.url
+                contentType: "video/mp4",
+                contentUrl: res.data.images.original.mp4
             }]);
             session.send(message);
 		});
