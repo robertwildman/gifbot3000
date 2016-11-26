220ee6752c4a4e26d658ce80ca339bcb0e756b33
Robert Wildman <Robertwildman4@gmail.com>
2016-11-26 22:54:03 +0000

    Adding Gif

--- messages/index.js
+++ messages/index.js
@@ -21,7 +21,7 @@ bot.dialog('/', function (session) {
 	var text = session.message.text;
 
 		giphy.translate(text, function(err, res) {
- 			var message = new builder.Message(session).addAttachments([{
+ 			var message = new builder.Message(session).message("GIF").addAttachments([{
                 contentType: "image/gif",
                 contentUrl: res.data.images.original.url
             }]);
